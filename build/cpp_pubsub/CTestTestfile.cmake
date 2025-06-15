ed289db520e9d42a39c5d118b578ba33ab33b043
Nihit13 <62506597+Nihit13@users.noreply.github.com>
2025-06-15 22:21:50 +0530

    Delete build directory

--- build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nihit/servcli/src/cpp_pubsub
-# Build directory: /home/nihit/servcli/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
